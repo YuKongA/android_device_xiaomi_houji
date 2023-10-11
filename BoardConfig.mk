489f197b3499b6872411353ae6bbe508be9fc58d
Arian <39061882+ArianK16a@users.noreply.github.com>
2023-10-11 19:31:54 +0800

    houji: Add lineage device framework manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,7 +135,9 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_SKUS := pineapple
 DEVICE_MANIFEST_PINEAPPLE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_pineapple.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.device.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := 
+    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.device.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
